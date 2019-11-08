b07ba29d9e99fc262eea979158d37862c6f444fb
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-11-07 16:43:45 -0800

    is_sorted ✅

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,9 +3,21 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    Running time: >>> O(n) because we have to iterate through the array. The bigger the array the longer it will take
+    Memory usage: >>> O(1) because we are not creating extra space """
+
+    # Declaring neighboring elements of items array
+    previous_element = 0
+    next_element = previous_element + 1
+
+    while previous_element < len(items) - 1:
+        # returning False at the first unsorted sequence
+        if items[previous_element] > items[next_element]:
+            return False
+        previous_element += 1
+        next_element += 1
+    return True
+
 
 
 def bubble_sort(items):
@@ -35,3 +47,8 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+
+
+if __name__ == '__main__':
+    items = [1, 2, 2, 4, 5, 6]
+    print(is_sorted(items))
