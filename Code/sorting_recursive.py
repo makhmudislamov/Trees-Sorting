abdb1e4a10d362070dd9b6a562a0d2b2f730e5db
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-14 14:25:44 -0800

    big O

--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -106,8 +106,8 @@ def partition(items, low, high):
     `[low...high]` by choosing a pivot (TODO: document your method here) from
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    Running time: ??? Why and under what conditions?
-    Memory usage: ??? Why and under what conditions?"""
+    Running time: O(logn) - assuming both halves of the arr are in the same len
+    Memory usage: O(1) - not creating any new arr"""
     # Choose a pivot any way and document your method in docstring above
     # Loop through all items in range [low...high]
     # Move items less than pivot into front of range [low...p-1]
