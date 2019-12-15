caa657f97e161cfad6af625bed2d64eda63ba671
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-12-15 10:42:04 -0800

    front and dequeue

--- Code/priorityqueue.py
+++ Code/priorityqueue.py
@@ -35,24 +35,26 @@ class PriorityQueue(object):
     def front(self):
         """Return the item at the front of this priority queue without removing
         it, or None if this priority queue is empty."""
-        if self.size() == 0:
+        if self.length() == 0:
             return None
-        # TODO: Return minimum item from heap
-        # ...
+        # Return minimum item from heap
+        return self.heap.get_min()
 
     def dequeue(self):
         """Remove and return the item at the front of this priority queue,
         or raise ValueError if this priority queue is empty."""
-        if self.size() == 0:
+        if self.length() == 0:
             raise ValueError('Priority queue is empty and has no front item')
-        # TODO: Remove and return minimum item from heap
-        # ...
+        # Remove and return minimum item from heap
+        front_item = self.heap.delete_min()
+        return front_item
+        
 
     def push_pop(self, item, priority):
         """Remove and return the item at the front of this priority queue,
         and insert the given item in order according to the given priority.
         This method is more efficient than calling dequeue and then enqueue."""
-        if self.size() == 0:
+        if self.length() == 0:
             raise ValueError('Priority queue is empty and has no front item')
         # TODO: Replace and return minimum item from heap
         # ...
